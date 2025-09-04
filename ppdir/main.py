eb843870ea0442ff616831f74683457fe53d860e
Matthew Hamilton <mwhamilton6@gmail.com>
2025-09-04 11:18:34 -0500

    Made a better default setter

--- ppdir/main.py
+++ ppdir/main.py
@@ -3,10 +3,6 @@ from typing import Any
 from .display import _display
 from .merge import get_info
 
-INCLUDE_DUNDERS = False
-INCLUDE_DOCS = True
-INCLUDE_SIGNATURES = False
-
 
 def defaults(
     *,
@@ -14,22 +10,20 @@ def defaults(
     include_docs: bool | None = None,
     include_signatures: bool | None = None,
 ) -> None:
-    global INCLUDE_DUNDERS, INCLUDE_DOCS, INCLUDE_SIGNATURES  # noqa: PLW0603
-    INCLUDE_DUNDERS = include_dunders if include_dunders is not None else INCLUDE_DUNDERS
-    INCLUDE_DOCS = include_docs if include_docs is not None else INCLUDE_DOCS
-    INCLUDE_SIGNATURES = include_signatures if include_signatures is not None else INCLUDE_SIGNATURES
+    ppdir.__kwdefaults__ = {
+        "include_dunders": include_dunders,
+        "include_docs": include_docs,
+        "include_signatures": include_signatures,
+    }
 
 
 def ppdir(
     inp_cls: Any,
     *,
-    include_dunders: bool | None = None,
-    include_docs: bool | None = None,
-    include_signatures: bool | None = None,
+    include_dunders: bool = False,
+    include_docs: bool = False,
+    include_signatures: bool = False,
 ) -> None:
-    include_dunders = include_dunders if include_dunders is not None else INCLUDE_DUNDERS
-    include_docs = include_docs if include_docs is not None else INCLUDE_DOCS
-    include_signatures = include_signatures if include_signatures is not None else INCLUDE_SIGNATURES
     if not isinstance(inp_cls, type):
         inp_cls = inp_cls.__class__
     class_summaries = get_info(inp_cls)
