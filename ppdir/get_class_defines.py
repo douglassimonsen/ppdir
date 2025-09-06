dea6f860b1cbfcf61e8c07f6dcdca606b1e429f3
Matthew Hamilton <mwhamilton6@gmail.com>
2025-09-06 16:00:21 -0500

    Fixed case where the MRO-class attributes weren't findable on the main class __attributes__ dictionary

--- ppdir/get_class_defines.py
+++ ppdir/get_class_defines.py
@@ -1,8 +1,10 @@
 def _expanded_dir(cls: type) -> list[str]:
     ret: list[str] = dir(cls)
     ret.extend(cls.__dict__.keys())
-    if "__annotations__" in cls.__dict__:
-        ret.extend(cls.__annotations__.keys())
+    for mro_cls in cls.mro():
+        # There appears to be cases where annotations are defined on the MRO classes, but not on the class itself
+        if "__annotations__" in mro_cls.__dict__:
+            ret.extend(mro_cls.__annotations__.keys())
     if "__pydantic_fields__" in cls.__dict__:
         # If a field exists, but isn't set by the instance, it doesn't appear in the dir() call
         ret.extend(cls.__pydantic_fields__.keys())
